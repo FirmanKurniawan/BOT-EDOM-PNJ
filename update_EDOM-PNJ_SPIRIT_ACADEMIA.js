900e07f1ed6ab7d843344f3d307ee01875a40a86
Firmansyah Helmi Kurniawan <kurniawan.firman94@gmail.com>
2024-01-30 11:47:21 +0700

    Update update_EDOM-PNJ_SPIRIT_ACADEMIA.js

--- update_EDOM-PNJ_SPIRIT_ACADEMIA.js
+++ update_EDOM-PNJ_SPIRIT_ACADEMIA.js
@@ -1,7 +1,8 @@
 //Change the value value="5" according to the value conditions | 1 Sangat Kurang | 2 Kurang | 3 Cukup | 4 Baik | 5 Sangat Baik
 
-const radioElements = document.querySelectorAll('div.form-check input[type="radio"][value="5"][disabled=""]');
+const radioElements = document.querySelectorAll('div.form-check input[type="radio"][value="5"]');
 radioElements.forEach((radioElement, index) => {
+    radioElement.removeAttribute('disabled');
     radioElement.checked = true;
     console.log(`Elemen radio ${index + 1}: Jawaban "Sangat Baik" dipilih.`);
 });
